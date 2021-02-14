1efde7b1451e3ffc47bd584e5c6ffa1248a0b4f3
tr0up2r <kmydtr@gmail.com>
2021-02-14 22:04:58 +0900

    동적 크기 배열 구현하기

--- array_practice/main.cpp
+++ array_practice/main.cpp
@@ -98,3 +98,45 @@ std::ostream& operator<<(std::ostream& os, const student& s)
 {
 	return (os << "[" << s.name << ". " << s.standard << "]");
 }
+
+
+
+int main(void)
+{
+	int nStudents;
+	std::cout << "1반 학생 수를 입력하세요 : ";
+	std::cin >> nStudents;
+
+	dynamic_array<student> class1(nStudents);
+	for (int i = 0; i < nStudents; i++)
+	{
+		std::string name;
+		int standard;
+		std::cout << i + 1 << "번째 학생 이름과 나이를 입력하세요 : ";
+		std::cin >> name >> standard;
+		class1[i] = student{ name, standard };
+	}
+
+	// 배열 크기보다 큰 인덱스의 학생에 접근
+	try
+	{
+		// 비정상 종료되는 상황
+		// class1[nStudents] = student{ "John", 8 };	// 예상할 수 없는 동작
+
+		class1.at(nStudents) = student{ "John", 8 };
+	}
+	catch (...)
+	{
+		std::cout << "예외 발생!" << '\n';
+	}
+
+	// 깊은 복사
+	auto class2 = class1;
+	std::cout << "1반을 복사하여 2반 생성 : " << class2.to_string() << '\n';
+
+	// 두 학급을 합쳐서 새로운 큰 학급을 생성
+	auto class3 = class1 + class2;
+	std::cout << "1반과 2반을 합쳐 3반 생성 : " << class3.to_string() << '\n';
+
+	return 0;
+}
